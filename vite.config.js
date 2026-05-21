b9951cc498e0f45744d8f3ccd39861ba67b67490
Maggy <natdanai@abbot.tech>
2026-05-21 14:27:30 +0700

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/NIT_Plus/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-antd':  ['antd', '@ant-design/icons'],
+        },
+      },
+    },
+  },
 })
